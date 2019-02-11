eedac0bf085be66944a9f44eb229fccf78310600
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2019-02-11 22:51:29 +0100

    Update encryptDecryptPassword.py

--- encryptDecryptPassword.py
+++ encryptDecryptPassword.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 # written by Heiko 11.02.2019
 
-import base64
+import base64, subprocess
 
 strPassword='8yaThaDCTGHxG8fu'
 
+##########################################################
+print ('Password: ' + strPassword)
+print ('')
+
+##########################################################
+## Using base64
 ## Function to encrypt password
 def encryptPw (strPassword):
 	return base64.b64encode(strPassword)
@@ -13,8 +19,32 @@ def encryptPw (strPassword):
 def decryprPw (strPassword):
 	return base64.b64decode(strPassword)
 
-myEncryptedPassword = encryptPw (strPassword)
-print ('My Encrypted Password is: ' + myEncryptedPassword)
+strEncrypted = encryptPw (strPassword)
+print ('Base64 encrypted password: ' + strEncrypted)
 
-myDecryptedPassword = decryprPw (myEncryptedPassword)
-print ('My Decrypted Password is: ' + myDecryptedPassword)
+strDecrypted = decryprPw (strEncrypted)
+print ('Base64 decrypted password: ' + strDecrypted)
+print ('')
+
+##########################################################
+## Using openssl
+# Function to encrypt password
+def encryptPwOpenSSL(inputString):
+	salt = subprocess.check_output(['/usr/bin/openssl', 'rand', '-hex', '8']).rstrip()
+	passphrase = subprocess.check_output(['/usr/bin/openssl', 'rand', '-hex', '12']).rstrip()
+	p = subprocess.Popen(['/usr/bin/openssl', 'enc', '-aes256', '-a', '-A', '-S', salt, '-k', passphrase], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+	encrypted = p.communicate(inputString)[0]
+	return encrypted, salt, passphrase
+	print("Encrypted String: %s" % encrypted)
+	
+# Function to decrypt password
+def decryptPwOpenSSL(inputString, salt, passphrase):
+	p = subprocess.Popen(['/usr/bin/openssl', 'enc', '-aes256', '-d', '-a', '-A', '-S', salt, '-k', passphrase], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+	return p.communicate(inputString)[0]
+	
+arrEncrypt = encryptPwOpenSSL(strPassword)
+print('OpenSSL encrypted password: ' + arrEncrypt[0])
+print('Salt: %s | Passphrase: %s' % (arrEncrypt[1], arrEncrypt[2]))
+
+strDecypted = decryptPwOpenSSL(arrEncrypt[0], arrEncrypt[1], arrEncrypt[2])
+print('OpenSSL encrypted password: ' + strDecypted)
