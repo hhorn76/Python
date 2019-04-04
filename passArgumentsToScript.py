77cdbc4d7569a71c90cf2ba8cf35fee366ca3a6e
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2019-04-04 12:59:09 +0200

    Create passArgumentsToScript.py

---
+++ passArgumentsToScript.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+print ('This is the name of the script: ',sys.argv[0])
+print ('This is argument one: ',sys.argv[1])
+print ('This is argument two: ',sys.argv[2])
+print ('This is argument three: ',sys.argv[3])
+print ('Number of arguments: ',len(sys.argv))
+print ('All arguments: ',str(sys.argv))
